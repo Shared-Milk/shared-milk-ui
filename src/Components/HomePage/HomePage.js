0afd579af1d9cde66556df5ed6293173a69f36f3
Jani Palomino <palomino.jani@gmail.com>
2022-03-24 23:46:13 -0600

    Build out home page

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,13 +1,27 @@
 import './HomePage.scss';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 const HomePage = () => {
   return (
-    <>
-    <Header />
-    <Footer />
-    </>
+    <main className='home'>
+      <aside className='mission'>
+        <h2>Our Mission</h2>
+        <p>To build connections between parents of baby’s in need and generous donors.</p>
+        <p>Additional messaging about our mission... </p>
+        <p>Message about providing resources and support..... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+      </aside>
+      <aside className='buttons'>
+        {/* will need "Link" the buttons below */}
+        <button className='need'>
+          Need Milk
+        </button>
+        <button className='donate'>
+          Donate Milk
+        </button>
+        <button className='support'>
+          Need Support
+        </button>
+      </aside>
+    </main>
   )
 };
 

db1b1d190d2a0cd93abb6e5888e11e55f0b60dfd
Jani Palomino <palomino.jani@gmail.com>
2022-03-25 00:28:53 -0600

    Update home page buttons to router Links

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import './HomePage.scss';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -10,16 +11,9 @@ const HomePage = () => {
         <p>Message about providing resources and support..... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
       </aside>
       <aside className='buttons'>
-        {/* will need "Link" the buttons below */}
-        <button className='need'>
-          Need Milk
-        </button>
-        <button className='donate'>
-          Donate Milk
-        </button>
-        <button className='support'>
-          Need Support
-        </button>
+        <Link exact to='/need-milk' className='need'>Need<br></br>Milk</Link>
+        <Link exact to='/donate' className='donate'>Donate<br></br>Milk</Link>
+        <Link exact to='/donate' className='support'>Need<br></br>Support</Link>
       </aside>
     </main>
   )

7929ce4eda23dd8afef9e4465c16915eeadadf8d
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-25 19:35:27 -0600

    Add tests for landing page user flow

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -1,5 +1,51 @@
-describe('Shared Milk user flows', () => {
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+describe('Shared Milk landing page user flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it('should display page title and navigation links on page load', () => {
+    cy.get('h1').contains('No Use Cryin\' Over Shared Milk')
+    cy.get('.nav').contains('Home')
+    cy.get('.nav').contains('Need Milk')
+    cy.get('.nav').contains('Donate')
+    cy.get('.nav').contains('About Us')
+    cy.get('.nav').contains('Support')
+  });
+
+  it('should be able to navigate to the Need Milk page view', () => {
+    // cy.intercept(fetch call)
+    cy.get('.nav').contains('Need Milk').click();
+    // cy.wait(['api call']);
+    cy.location().should((location => {
+      expect(location.href).to.eq('http://localhost:3000/need-milk')
+    }));
+
+    it('should be able to navigate to the Donate page form', () => {
+      // cy.intercept(fetch call)
+      cy.get('.nav').contains('Donate').click();
+      // cy.wait(['api call']);
+      cy.location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/donate')
+      }));
+    });
+
+    it('should be able to navigate to the About Us page view', () => {
+      // cy.intercept(fetch call)
+      cy.get('.nav').contains('About Us').click();
+      // cy.wait(['api call']);
+      cy.location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/about-us')
+      }));
+    });
+
+    it('should be able to navigate to the Support page view', () => {
+      // cy.intercept(fetch call)
+      cy.get('.nav').contains('Support').click();
+      // cy.wait(['api call']);
+      cy.location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/support')
+      }));
+    });
+
   });
 });

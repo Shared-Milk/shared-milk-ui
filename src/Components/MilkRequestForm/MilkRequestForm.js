ec32a97eb5d0bcacffcac7463e06828b7a7c9598
Jani Palomino <palomino.jani@gmail.com>
2022-03-26 00:05:10 -0600

    Update MilkRequestForm component styling

--- src/Components/MilkRequestForm/MilkRequestForm.js
+++ src/Components/MilkRequestForm/MilkRequestForm.js
@@ -5,16 +5,15 @@ const MilkRequestForm = () => {
     <section className='main-container'>
       <h2>Connect with Sarah</h2>
       <form>
-        <label for='First Name'>First Name</label>
-        <input type='text' placeholder='First Name' />
-        <label for='Last Name'>Last Name</label>
-        <input type='text' placeholder='Last Name' />
+        <label for='First Name'>Your Name</label>
+        <input type='text' placeholder='First & Last Name' />
         <label for='Email Address'>Email Address</label>
         <input type='email' placeholder='Email Address' />
         <label for='Phone Number'>Phone Number</label>
         <input type='tel' placeholder='Phone Number' />
+        <label for='Message'>Message</label>
         <textarea placeholder='How much milk do you need? When do you need it? Tell your donor more about yourself.' />
-        <button type='submit'>Submit</button>
+        <button type='submit' className='button'>Submit</button>
       </form>
     </section>
   )

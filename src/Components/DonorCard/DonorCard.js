e5cd288b7b754ff35c368095fb1b520064c1f045
Jani Palomino <palomino.jani@gmail.com>
2022-03-25 21:45:42 -0600

    Add donor card component and styling

--- src/Components/DonorCard/DonorCard.js
+++ src/Components/DonorCard/DonorCard.js
@@ -1,4 +1,5 @@
 import './DonorCard.scss';
+import { Link } from 'react-router-dom';
 
 const DonorCard = () => {
   //let display = donors.map(donor => {
@@ -10,13 +11,16 @@ const DonorCard = () => {
   ///>
   // })
   return (
-    <article>
-      <h3>Sarah Smith</h3>
-      <p>Age: 28</p>
-      <p>City: Denver</p>
-      <h4>Bio</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-    </article>
+    <section className='contact-grid'>
+      {/* will need to update the below link to a specific contact id */}
+      <Link to='/contact' className='contact-card'>
+        {/* Will need to fill in cards below with dummy data */}
+        <h3>Sarah Smith</h3>
+        <p>Location: Denver, CO</p>
+        <h4>Bio</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+      </Link>
+    </section>
   )
 };
 

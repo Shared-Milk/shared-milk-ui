bc493b8aec2b8e5ae26e15671f9fecab79cd30cb
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-27 11:28:22 -0600

    Edit mission statement

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -6,11 +6,12 @@ const HomePage = () => {
     <section className='main-container columns'>
       <aside className='mission'>
         <h2>Our Mission</h2>
-        <p>Our orgnanization strives to build connections between parents of babies in need and generous donors.</p>
+        <p>Our organization strives to build connections between parents of babies in need and generous donors.</p>
         <p>Human milk is the natural food for babies and provides them with optimal nutrition that allows them to reach their health potential.
            We are here to help mothers who are not able to nurse their babies or produce enough milk by connecting them with mothers who produce more milk than their babies need. 
-           Together, we will help infants thrive by making donated breastmilk safe and accessible.</p>
-        <p>We understand that breastfeeding issues can lead to many different stressers and emotions. We are here for you and you are not alone. Please visit our Support page to view more resources.</p>
+           Together, we will help infants thrive by making donated breastmilk accessible.</p>
+        <p>The period of time after a baby is born is naturally a very stressful time. Emotions and pressure are high. We understand that breastfeeding issues can be a major contributor to a mother's stress level.
+           We are here for you and you are not alone. Please visit our <Link exact to='/support'>Support Page</Link> to view more resources.</p>
       </aside>
       <aside className='buttons'>
         <Link exact to='/need-milk' className='need'>Need<br></br>Milk</Link>

c12f3680ad03f1c154f02337af72f7930463ff29
Devon Wigle <devonwigle@gmail.com>
2022-03-27 13:39:19 -0500

    Make functional navigation for smoker option form submit

--- src/Components/DonateMilkForm/DonateMilkForm.js
+++ src/Components/DonateMilkForm/DonateMilkForm.js
@@ -1,14 +1,24 @@
 import './DonateMilkForm.scss';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const DonateMilkForm = () => {
-  function handleDonorSubmit() {
-    if (document.getElementById('yes').checked) {
-      return <Link exact to='/sorry'></Link>
-    } else {
-      <Link exact to='/thank-you'>Submit</Link>
+  const [smoker, setSmoker] = useState()
+  const handleSmoker = (event) => {
+    setSmoker(event.target.value)
+    console.log(smoker)
+  }
+
+  const navigate = useNavigate();
+
+  const handleDonorSubmit = () => {
+    if (smoker === 'yes') {
+      return navigate('/sorry')
+    } else if (smoker === 'no') {
+      return navigate('/thank-you')
     }
   }
+  
   return (
     <form className='donor-form'>
       <label htmlFor='Your Name'>Your Name</label>
@@ -23,22 +33,14 @@ const DonateMilkForm = () => {
       <input type='text' placeholder='State' />
       <p className='form-question'>Have you used any tobacco products in the last 6 weeks?</p>
       <div className='form-group'>
-        <label for='yes'>YES</label>
-        <input type='radio' name='smoker' value='yes' id='yes'/>
-        <label for='no'>NO</label>
-        <input type='radio' name='smoker' value='no' id='no'/>
+        <label htmlFor='yes'>YES</label>
+        <input type='radio' name='smoker' value='yes' onChange={(event) => {handleSmoker(event)}}/>
+        <label htmlFor='no'>NO</label>
+        <input type='radio' name='smoker' value='no' id='no' onChange={(event) => { handleSmoker(event)}}/>
       </div>
       <label htmlFor='message'>Message</label>
       <textarea placeholder='Tell us about yourself. Why are you donating? How much milk do you have available?' />
-      {/* 
-      
-      link below needs a conditional that depends on the 'yes' or 'no' radio buttons above 
-
-      - link to '/sorry' if YES
-      - linke to '/thank-you' if NO
-      
-      */}
-      <button className='button' onSubmit={handleDonorSubmit()}>Submit</button>
+      <button className='button' onClick={() => handleDonorSubmit()}>Submit</button>
     </form>
   )
 };

7c7eda17a37cdb45197b495cac36b6ee0f08c1ae
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-29 17:21:58 -0600

    Fix compiling errors and imports

--- src/Graphql/Mutations.js
+++ src/Graphql/Mutations.js
@@ -1,36 +1,38 @@
 import { gql } from "@apollo/client"
 
-// export const CREATE_DONOR = gql`
-// mutation createUser(
-//   $name: String,
-//   $email: String,
-//   $phone: String,
-//   $bio: String,
-//   $location: String) {
-//   queryUsers(
-    // name: $name,
-    // email: $email,
-    // phone: $phone,
-    // bio: $bio,
-    // location: $location
-//   ) {
-//     id
-//     }
-// }`
-
 export const CREATE_DONOR = gql`
-mutation 
-  createUser($input: 
-    {
-      name: $name,
-      email: $email,
-      phone: $phone,
-      bio: $bio,
-      location: $location
-      donor_status: "0"
-    }) {
-      mom: {
-          id  
-      }
-      errors
-    }`
+mutation createUser(
+  $name: String,
+  $email: String,
+  $phone: String,
+  $bio: String,
+  $location: String
+  $donor_status: Int) {
+  queryUsers(
+    name: $name,
+    email: $email,
+    phone: $phone,
+    bio: $bio,
+    location: $location
+    donor_status: $donor_status
+  ) {
+    id
+    }
+}`
+
+// export const CREATE_DONOR = gql`
+// mutation 
+//   createUser($input: 
+//     {
+//       name: $name,
+//       email: $email,
+//       phone: $phone,
+//       bio: $bio,
+//       location: $location
+//       donor_status: "0"
+//     }) {
+//       mom: {
+//           id  
+//       }
+//       errors
+//     }`

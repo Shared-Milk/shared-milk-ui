40d616226caa3939aba896891cf89876af4ac8c7
lschnell8 <lschnell8@gmail.com>
2022-03-28 19:54:26 -0600

    Build graphql mutation structure

---
+++ src/Graphql/Mutations.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client"
+
+export const CREATE_DONOR = gql`
+mutation createUser(
+  $name: String,
+  $email: String,
+  $phone: String,
+  $bio: String,
+  $location: String) {
+  queryUsers(
+    name: $name,
+    email: $email,
+    phone: $phone,
+    bio: $bio,
+    location: $location
+  ) {
+    id
+    }
+}`
+  

4f0825d9af717eed6d5fc703d49f6c8a50c283e3
lschnell8 <lschnell8@gmail.com>
2022-03-29 16:12:44 -0600

    Troubleshoot npm form package

--- src/Components/MilkRequestForm/MilkRequestForm.js
+++ src/Components/MilkRequestForm/MilkRequestForm.js
@@ -6,54 +6,67 @@ import { useForm } from 'react-hook-form';
 const MilkRequestForm = () => {
   const location = useLocation();
   const name = location.state.name || 'user';
-  const [recipientName, setRecipientName] = useState();
-  const [recipientEmail, setRecipientEmail] = useState();
-  const [recipientPhone, setRecipientPhone] = useState();
-  const [recipientMessage, setRecipientMessage] = useState();
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  // const [recipientName, setRecipientName] = useState();
+  // const [recipientEmail, setRecipientEmail] = useState();
+  // const [recipientPhone, setRecipientPhone] = useState();
+  // const [recipientMessage, setRecipientMessage] = useState();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      recipientName: '',
+      recipientEmail: '',
+      recipientPhone: '',
+      recipientMessage: ''
+    }
+    });
+  const onSubmit = data => {console.log(data)};
+  console.log(errors)
+  // console.log(onValid)
 
-
-  const handleChange = (event) => {
-    if (event.target.name === 'recipientName') {
-      setRecipientName(event.target.value)
-      return recipientName
-    }
-    if (event.target.name === 'recipientEmail') {
-      setRecipientEmail(event.target.value)
-      return recipientEmail
-    }
-    if (event.target.name === 'recipientPhone') {
-      setRecipientPhone(event.target.value)
-      return recipientPhone
-    }
-    if (event.target.name === 'recipientMessage') {
-      setRecipientMessage(event.target.value)
-      return recipientMessage
-    }
-  }
+  // const handleChange = (event) => {
+  //   if (event.target.name === 'recipientName') {
+  //     setRecipientName(event.target.value)
+  //     return recipientName
+  //   }
+  //   if (event.target.name === 'recipientEmail') {
+  //     setRecipientEmail(event.target.value)
+  //     return recipientEmail
+  //   }
+  //   if (event.target.name === 'recipientPhone') {
+  //     setRecipientPhone(event.target.value)
+  //     return recipientPhone
+  //   }
+  //   if (event.target.name === 'recipientMessage') {
+  //     setRecipientMessage(event.target.value)
+  //     return recipientMessage
+  //   }
+  // }
 
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
-    navigate('sent-confirmation')
-  }
+  // const handleNavigation = () => {
+  //   // onSubmit()
+  //   navigate('sent-confirmation')
+  // }
 
 
   return (
     <section className='main-container'>
       <h2>Connect with { name }</h2>
-      <form>
-        <label htmlFor='First Name'>Your Name</label>
-        <input type='text' name='recipientName' onChange={ (event) => handleChange(event) } placeholder='First & Last Name' />
+      <form onSubmit={ handleSubmit(!errors && navigate('sent-confirmation')) }>
+        <label htmlFor='First & Last Name'>Your Name</label>
+        <input {...register('recipientName', { required: 'This field required' })} type='text' placeholder='First & Last Name' />
+        <p>{ errors.recipientName?.message }</p>
         <label htmlFor='Email Address'>Email Address</label>
-        <input type='email' placeholder='Email Address' onChange={ (event) => { handleChange(event) } } />
+        <input {...register('recipientEmail', {required: 'This field required'})} type='email' placeholder='Email Address' />
+        <p>{ errors.recipientEmail?.message }</p>
         <label htmlFor='Phone Number'>Phone Number</label>
-        <input type='tel' name='recipientPhone' onChange={ (event) => handleChange(event) } placeholder='Phone Number' />
+        <input {...register('recipientPhone', { required: 'This field required' })} type='tel' placeholder='Phone Number' />
+        <p>{ errors.recipientPhone?.message }</p>
         <label htmlFor='Message'>Message</label>
-        <textarea name='recipientMessage' onChange={ (event) => handleChange(event) } placeholder='How much milk do you need? When do you need it? Tell your donor more about yourself.' />
+        <textarea {...register('recipientMessage', { required: 'This field required' })} placeholder='How much milk do you need? When do you need it? Tell your donor more about yourself.' />
+        <p>{ errors.recipientMessage?.message }</p>
         {/* <Link to='/sent-confirmation' className='button'>Send Message</Link> */}
-        <button className='button' onClick={ () => handleSubmit() }>Submit</button>
+        <button className='button' onSubmit={onSubmit} type='submit'>Submit</button>
       </form>
     </section>
   )

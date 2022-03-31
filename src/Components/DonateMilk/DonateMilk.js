9a4490a91347933e9bdc8da5d415cbdd6f460881
Devon Wigle <devonwigle@gmail.com>
2022-03-31 15:57:30 -0500

    Add test ids to the donate milk page

--- src/Components/DonateMilk/DonateMilk.js
+++ src/Components/DonateMilk/DonateMilk.js
@@ -2,8 +2,8 @@ import DonateMilkForm from '../DonateMilkForm/DonateMilkForm';
 
 const DonateMilk = () => {
   return (
-    <section className='main-container donate'>
-      <h2>Donate Milk</h2>
+    <section data-testid='donate-container' className='main-container donate'>
+      <h2 data-testid='donate-title'>Donate Milk</h2>
       <p>There’s a high demand for breastmilk, and we’re working quickly to distribute to as many infants as we can. Your donations are greatly appreciated and needed and help us achieve our goal of providing this service to babies in need.</p>
       <p>Thank you for considering donating. Please complete the application below to be added to our donor page.</p>
       <hr />

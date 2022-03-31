13023b2004d7e1474522e4932796058a137b8881
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-31 14:34:49 -0600

    Add proptypes to DonorCard

--- src/Components/DonorCard/DonorCard.js
+++ src/Components/DonorCard/DonorCard.js
@@ -1,11 +1,11 @@
 import './DonorCard.scss';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
 
 const DonorCard = ({donor}) => {
   return (
     <>
       <Link to="/contact" state={donor} className='contact-card'>
-        {/* Will need to fill in cards below with dummy data */}
         <h3>{donor.name}</h3>
         <p>Location: {donor.location}</p>
         <h4>Bio</h4>
@@ -16,3 +16,11 @@ const DonorCard = ({donor}) => {
 };
 
 export default DonorCard;
+
+DonorCard.propTypes = {
+  allBooks: PropTypes.shape({
+    name: PropTypes.string,
+    location: PropTypes.string,
+    bio: PropTypes.string
+  })
+}

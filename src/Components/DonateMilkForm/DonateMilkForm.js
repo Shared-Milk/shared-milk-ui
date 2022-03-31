1e763c248aa80b6b9ae2fc238e289da7eef4374f
Jani Palomino <palomino.jani@gmail.com>
2022-03-31 17:00:32 -0600

    Remove commented out code

--- src/Components/DonateMilkForm/DonateMilkForm.js
+++ src/Components/DonateMilkForm/DonateMilkForm.js
@@ -97,21 +97,19 @@ const DonateMilkForm = () => {
   return (
     <form className='donor-form'>
       <label htmlFor='Your Name'>Your Name</label>
-      <input type='text' placeholder='First & Last Name'  onChange={(event) => { handleDonorName(event) }}/>
+      <input type='text' placeholder='First & Last Name' onChange={(event) => { handleDonorName(event) }}/>
       <label htmlFor='Email Address'>Email Address</label>
-      <input type='email' placeholder='Email Address'  onChange={(event) => { handleDonorEmail(event) }} />
+      <input type='email' placeholder='Email Address' onChange={(event) => { handleDonorEmail(event) }} />
       <label htmlFor='Phone Number'>Phone Number</label>
-      <input type='tel' placeholder='Phone Number'  onChange={(event) => { handleDonorPhone(event) }} />
+      <input type='tel' placeholder='Phone Number' onChange={(event) => { handleDonorPhone(event) }} />
       <label htmlFor='Location'>Location</label>
-      <input type='text' placeholder='Location'  onChange={(event) => { handleDonorLocation(event) }}/>
-      {/* <label htmlFor='State'>State</label>
-      <input type='text' placeholder='State' onChange={(event) => { handleDonorState(event) }}/> */}
+      <input type='text' placeholder='Location' onChange={(event) => { handleDonorLocation(event) }}/>
       <p className='form-question'>Have you used any tobacco products in the last 6 weeks?</p>
       <div className='form-group'>
-        <label htmlFor='yes'>YES</label>
-        <input type='radio' name='smoker' value='yes'  onChange={(event) => {handleSmoker(event)}}/>
         <label htmlFor='no'>NO</label>
         <input type='radio' name='smoker' value='no' id='no' onChange={(event) => { handleSmoker(event)}}/>
+        <label htmlFor='yes'>YES</label>
+        <input type='radio' name='smoker' value='yes' onChange={(event) => {handleSmoker(event)}}/>
       </div>
       <label htmlFor='message'>Message</label>
       <textarea type='text' placeholder='Tell us about yourself. Why are you donating? How much milk do you have available?'  onChange={(event) => { handleDonorBio(event) }}/>

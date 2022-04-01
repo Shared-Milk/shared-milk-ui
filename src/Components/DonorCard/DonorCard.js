b3111df12d0bb0158d12a76b3ee6746628cc4655
lschnell8 <lschnell8@gmail.com>
2022-04-01 01:47:21 -0600

    Adjust formatting for consistency

--- src/Components/DonorCard/DonorCard.js
+++ src/Components/DonorCard/DonorCard.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 
 const DonorCard = ({ donor }) => {
   return (
-    <>
-      <Link to="/contact" state={donor} className='contact-card'>
-        <h3>{ donor.name }</h3>
-        <p>Location: { donor.location }</p>
-        <h4>Bio</h4>
-        <p>{ donor.bio }</p>
-      </Link>
-    </>
+    <Link to="/contact" state={ donor } className='contact-card'>
+      <h3>{ donor.name }</h3>
+      <p>Location: { donor.location }</p>
+      <h4>Bio</h4>
+      <p>{ donor.bio }</p>
+    </Link>
   )
 };
 

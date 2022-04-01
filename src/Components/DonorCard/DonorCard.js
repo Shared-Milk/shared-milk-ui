7620d07b0d55bed27e6b29c945fa6ccdf7398b2e
lschnell8 <lschnell8@gmail.com>
2022-04-01 00:38:30 -0600

    Clean up unused code and unify syntax style in DonorCard file

--- src/Components/DonorCard/DonorCard.js
+++ src/Components/DonorCard/DonorCard.js
@@ -5,7 +5,6 @@ const DonorCard = ({ donor }) => {
   return (
     <>
       <Link to="/contact" state={donor} className='contact-card'>
-        {/* Will need to fill in cards below with dummy data */}
         <h3>{ donor.name }</h3>
         <p>Location: { donor.location }</p>
         <h4>Bio</h4>

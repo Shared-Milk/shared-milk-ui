23489b8cfbb856104f90544be98612951862dec0
lschnell8 <lschnell8@gmail.com>
2022-04-01 00:40:56 -0600

    Clean up unused code and unify syntax style in MobileNav

--- src/Components/MobileNav/MobileNav.js
+++ src/Components/MobileNav/MobileNav.js
@@ -17,21 +17,3 @@ const MobileNav = () => {
 };
 
 export default MobileNav;
-
-// class Example extends React.Component {
-//   showSettings (event) {
-//     event.preventDefault();
-//   }
-
-//   render () {
-//     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
-//     return (
-//       <Menu>
-//         <a id="home" className="menu-item" href="/">Home</a>
-//         <a id="about" className="menu-item" href="/about">About</a>
-//         <a id="contact" className="menu-item" href="/contact">Contact</a>
-//         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-//       </Menu>
-//     );
-//   }
-// }

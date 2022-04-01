bbb6d0ca00cd165d82ec0c690f42192c5e31ae7a
lschnell8 <lschnell8@gmail.com>
2022-04-01 00:42:22 -0600

    Clean up unused code and unify syntax style in Mutations

--- src/Graphql/Mutations.js
+++ src/Graphql/Mutations.js
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client"
+import { gql } from '@apollo/client';
 
 export const CREATE_DONOR = gql`
 
@@ -25,52 +25,3 @@ mutation CreateUser(
   }
 }
 `;
-// mutation{
-//   createUser(input: {
-//     name: String,
-//     email: String,
-//     phone: String,
-//     bio: String,
-//     location: String,
-//     donorStatus: Int}) {
-//     createUser(
-//       name: name,
-//       email: email,
-//       phone: phone,
-//       bio: bio,
-//       location: location
-//       donorStatus: donorStatus
-//     ) {
-//       id
-//       }
-//       errors
-//     }
-// }`
-
-
-
-// mutation CreateUser($input: CreateUserInput!) {
-//   createUser (input: $input) {
-//     name: String,
-//     email: String,
-//     phone: String,
-//     bio: String,
-//     location: String,
-//     donorStatus: Int})
-//   }
-// export const CREATE_DONOR = gql`
-// mutation 
-//   createUser($input: 
-//     {
-//       name: $name,
-//       email: $email,
-//       phone: $phone,
-//       bio: $bio,
-//       location: $location,
-//       donorStatus: 0
-//     }) {
-//       user {
-//           id  
-//       }
-//       errors
-//     }`

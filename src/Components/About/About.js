bdfa95652b10c717578b2e0b96aa02a4f576e0f6
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-01 15:22:09 -0600

    Fix heroku

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className='main-container'>
       <h2>About Us</h2>
-      <p>This is Jani's pet project to include our photos, bios, linked in profiles, githubs, etc...</p>
+      <p>This is Jani's pet project to include our photos, bios, linked in profiles, githubs, etc...and then someeeeee....</p>
     </section>
   )
 };

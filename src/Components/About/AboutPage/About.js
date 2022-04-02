474e0b73f85916c419866c0b0a8c73ba678a47ee
Jani Palomino <palomino.jani@gmail.com>
2022-04-02 17:41:56 -0600

    Update inspiration and mission statements and styling

--- src/Components/About/AboutPage/About.js
+++ src/Components/About/AboutPage/About.js
@@ -15,10 +15,15 @@ const About = () => {
   return (
     <section className='main-container about-page'>
       <h2>Our Inspiration</h2>
-      <p>Our inspiration for this application is to connect families with newborns in need of breastmilk with mothers or families of mothers who over produce milk.</p>
-      <p>This is a Turing Module 4 project built with React.js, Ruby, Rails, GraphQL, and .... </p>
-      <p>Click on any of us below to get to know more about us.</p>
+        <p>"No Use Cryin' Over Shared Milk" was inspired by our founder, Seth Perna's, personal family battle with producing breast milk for his son and the mental an financial struggle that followed. Seth's mission became our mission...</p>
+        <div className='mission-container'>
+          <p className='mission'>"to create a save space for families who under-produce breast milk to connect with families who over-produce."</p>
+        </div>
+      <h3>Our Tech Stack</h3>
+        <p>This is a Turing Module 4 project built with React.js, JavaScript, HTML, SASS, Ruby, Rails, GraphQL, Cypress, and CircleCI. </p>
       <section className='team-container'>
+        <h3>Get to Know Us</h3>
+          <p>Click on any photo below to get to know more about us.</p>
         <TeamMembers team={team} />
       </section>
     </section>
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -6,14 +6,9 @@ const HomePage = () => {
     <section className='main-container columns'>
       <aside className='mission'>
         <h2>Our Mission</h2>
-          <p>To create a safe space for families who under-produce breast milk to connect with families that over-produce.</p>
+          <p>To create a safe space for families who under-produce breast milk to connect with families who over-produce.</p>
           <p>We keep the money out of it as a donation only based program.</p>
           <p>Whether you are a parent in need, or are here to donate, we hope you are able to build a connection with another family and join us in our mission to help infants thrive by making donated milk accessible.</p>
-        {/* <p>To build connections between parents of babies in need of milk and generous donors.</p>
-        <p>Human milk is the natural food for babies and provides them with optimal nutrition that allows them to reach their health potential.
-           We are here to help families who are not able to nurse their babies or produce enough milk by connecting them with mothers who produce more milk than their babies need. 
-           Together, we will help infants thrive by making donated breastmilk accessible.</p>
-        <p>The period of time after a baby is born is naturally a very stressful time. Emotions and pressure are high. We understand that breastfeeding issues can be a major contributor to a mother's stress level. */}
       </aside>
       <aside className='buttons'>
         <Link to='/need-milk' className='need'>Need<br></br>Milk</Link>

77134312595be574006bc6fbf4b15d837344c144
Jani Palomino <palomino.jani@gmail.com>
2022-04-02 15:43:56 -0600

    Update header to reflect 'About' instead of 'About Us' for new messaging

--- src/Components/About/AboutPage/About.js
+++ src/Components/About/AboutPage/About.js
@@ -14,7 +14,7 @@ const About = () => {
 
   return (
     <section className='main-container about-page'>
-      <h2>About Us</h2>
+      <h2>Our Inspiration</h2>
       <p>Our inspiration for this application is to connect families with newborns in need of breastmilk with mothers or families of mothers who over produce milk.</p>
       <p>This is a Turing Module 4 project built with React.js, Ruby, Rails, GraphQL, and .... </p>
       <p>Click on any of us below to get to know more about us.</p>

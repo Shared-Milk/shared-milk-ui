8ecd88af6d06221329376b604c466779a0b86814
Jani Palomino <palomino.jani@gmail.com>
2022-04-02 02:36:33 -0600

    Remove unused code

--- src/Components/About/MemberCards/MemberCard.js
+++ src/Components/About/MemberCards/MemberCard.js
@@ -5,10 +5,7 @@ import { Link, Routes, Route } from 'react-router-dom';
 import Bio from '../ProfilePage/Devon';
 import MemberPage from '../ProfilePage/Profile';
 
-// const memberCard = ({id, name, photo, pronouns, linkedin, github, bio}) => {
-  const memberCard = ({member}) => {
-    // console.log('MemberCard member', member)
-
+const memberCard = ({member}) => {
   return (
     <article className='member-card'>
       <Link to={`${member.name}`} state={member}>
@@ -19,14 +16,8 @@ import MemberPage from '../ProfilePage/Profile';
         <img className='social-icon' src={linkedinIcon} alt={`${member.name}'s Linked In Profile`}/>
         <img className='social-icon' src={githubIcon} alt={`${member.name}'s Github Profile`}/>
       </div>
-
-      {/* <Routes>
-        <Route path=':id' element={<MemberPage />}/>
-      </Routes> */}
     </article>
   )
 }
 
 export default memberCard;
-
-{/* <DonorCard key={donor.id} donor={donor}/> */}
--- src/Components/About/TeamMembers/TeamMembers.js
+++ src/Components/About/TeamMembers/TeamMembers.js
@@ -5,37 +5,13 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 
 const TeamMembers = ({team}) => {
-  // const memberDetails = team.map(teamMember => {
-  //   return (
-  //     <MemberCard 
-  //       key={teamMember.id}
-  //       id={teamMember.id}
-  //       name={teamMember.name}
-  //       photo={teamMember.photo}
-  //       pronouns={teamMember.pronouns}
-  //       linkedin={teamMember.linkedin}
-  //       github={teamMember.github}
-  //       bio={teamMember.bio}
-  //     />
-  //   )
-  // });
-
   return (
     <section className='team-grid'>
-      {/* {memberDetails} */}
-      {/* <MemberCard key={teamMember.id} member={teamMember}/> */}
       {team.map(teamMember => {
         return (
           <MemberCard 
             key={teamMember.id}
             member={teamMember}
-            // id={teamMember.id}
-            // name={teamMember.name}
-            // photo={teamMember.photo}
-            // pronouns={teamMember.pronouns}
-            // linkedin={teamMember.linkedin}
-            // github={teamMember.github}
-            // bio={teamMember.bio}
           />
         )
       })}

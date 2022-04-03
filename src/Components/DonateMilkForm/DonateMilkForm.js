02e91fb1a3472805e52500b94c78bd26d137f913
Devon Wigle <devonwigle@gmail.com>
2022-04-02 20:39:49 -0500

    Add test ids for form error messages

--- src/Components/DonateMilkForm/DonateMilkForm.js
+++ src/Components/DonateMilkForm/DonateMilkForm.js
@@ -46,13 +46,13 @@ const DonateMilkForm = () => {
     <form className='donor-form' onSubmit={ handleSubmit(onSubmit) }>
       <label htmlFor='Your Name'>Your Name</label>
       <input data-testid='donor-name-input' {...register('donorName', { required: `* Is the baby-brain that bad?` })} type='text' placeholder='First & Last Name' />
-      <p className='error'>{ errors.donorName?.message }</p>
+      <p data-testid='donor-name-error' className='error'>{ errors.donorName?.message }</p>
       <label htmlFor='Email Address'>Email Address</label>
       <input data-testid='donor-email-input' {...register('donorEmail', { required: `* Oh, fer cryin' out loud. We don't do snail mail. Just enter your email` })} type='email' placeholder='Email Address' />
-      <p className='error'>{ errors.donorEmail?.message }</p>
+      <p data-testid='donor-email-error' className='error'>{ errors.donorEmail?.message }</p>
       <label htmlFor='Phone Number'>Phone Number</label>
       <input data-testid='donor-phone-input' {...register('donorPhone', { required: `* Can I have yo number??` })} type='tel' placeholder='Phone Number' />
-      <p className='error'>{ errors.donorPhone?.message }</p>
+      <p data-testid='donor-phone-error' className='error'>{ errors.donorPhone?.message }</p>
       <label htmlFor='Location'>Location</label>
       <input data-testid='donor-location-input' {...register('donorLocation', { required: `Ope! We can't find you. Peekaboo!` })} type='text' placeholder='Location' />
       <p data-testid='donor-radio-descriptor' className='form-question'>Have you used any tobacco products in the last 6 weeks?</p>
@@ -62,11 +62,11 @@ const DonateMilkForm = () => {
         <label htmlFor='yes'>YES</label>
         <input data-testid='donor-smoker-yes' {...register('smoker', { required: `* Did you go out for a smoke?` })} type='radio' name='smoker' value='1' />
       </div>
-      <p className='error'>{ errors.smoker?.message }</p>
+      <p data-testid='donor-smoker-error' className='error'>{ errors.smoker?.message }</p>
       <label htmlFor='message'>Message</label>
       <textarea data-testid='donor-bio-input' {...register('donorBio', { required: `* Seriously though, tell us about yourself. Don't be sketchy` })} type='text' placeholder='Tell us about yourself. Why are you donating? How much milk do you have available?' />
-      <p className='error'>{ errors.donorBio?.message }</p>
-      <button className='button' type='submit'>Submit</button>
+      <p data-testid='donor-bio-error' className='error'>{ errors.donorBio?.message }</p>
+      <button data-testid='donate-submit-button' className='button' type='submit'>Submit</button>
     </form>
   )
 };

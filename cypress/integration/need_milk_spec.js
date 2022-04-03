5740060bebb75473600625fd19cea8b91a6a8161
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-03 12:36:12 -0600

    Add data test ids

--- cypress/integration/need_milk_spec.js
+++ cypress/integration/need_milk_spec.js
@@ -11,40 +11,40 @@ describe('need milk donor results page user flow', () => {
   })
 
   it('should have a donor results container that displays three donors', () => {
-    cy.get('.main-container').should('exist')
-      .get('.contact-grid')
-      .get('a.contact-card').should('have.length', 3)
+    cy.get('[data-testid=main-container]').should('exist')
+      .get('[data-testid=contact-grid]')
+      .get('[data-testid=contact-card]').should('have.length', 3)
   })
-
+ 
   it('should display donor information on each donor card', () => {
-    cy.get('a.contact-card').first()
-      .get('h3').contains('Stacey Berkowitz')
-      .get('p').contains('Location: Denver, CO')
-      .get('h4').contains('Bio')
-      .get('p').contains('I want to help struggling mothers and hungry babies')
+    cy.get('[data-testid=contact-card]').first()
+      .get('[data-testid=donor-name]').contains('Stacey Berkowitz')
+      .get('[data-testid=donor-location]').contains('Location: Denver, CO')
+      .get('[data-testid=bio-title]').contains('Bio')
+      .get('[data-testid=donor-bio]').contains('I want to help struggling mothers and hungry babies')
   })
 
   it('should allow user to click on donor that they want to contact', () => {
-    cy.get('a.contact-card').first().click()
+    cy.get('[data-testid=contact-card]').first().click()
     cy.location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/contact')
     }));
-    cy.get('h2').contains('Connect with Stacey Berkowitz')
-    cy.get('form')
-      .get('.main-container')
-    cy.get('.name')
+    cy.get('[data-testid=request-container]')
+    cy.get('[data-testid=donor-connect]').contains('Connect with Stacey Berkowitz')
+    cy.get('[data-testid=request-form]')
+    cy.get('[data-testid=request-name]')
       .type('Joan Russell')
       .should('have.value', 'Joan Russell')
-      cy.get('.email')
+      cy.get('[data-testid=request-email]')
       .type('j_russell@gmail.com')
       .should('have.value', 'j_russell@gmail.com')
-      cy.get('.tel')
+      cy.get('[data-testid=request-tel]')
       .type('3108101022')
       .should('have.value', '3108101022')
-    cy.get('textarea')
+    cy.get('[data-testid=request-bio]')
       .type('I need extra milk to feed my growing boy, Teddy')
       .should('have.value', 'I need extra milk to feed my growing boy, Teddy')
-    cy.get('.button').click()
+    cy.get('[data-testid=request-button]').click()
     cy.location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/sent-confirmation')
     }));
@@ -52,21 +52,20 @@ describe('need milk donor results page user flow', () => {
 
   it('should display a confirmation message once user clicks submit on milk request form', () => {
     cy.visit('http://localhost:3000/sent-confirmation')
-    cy.get('h2').contains('Message Sent')
-      .get('p').contains('Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.')
-      .get('p').contains('Best wishes!')
+    cy.get('[data-testid=confirmation]').contains('Message Sent')
+      .get('[data-testid=confirmation-msg]').contains('Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.')
+      .get('[data-testid=confirmation-wishes]').contains('Best wishes!')
   })
 
   it('should not let a user connect with a donor if they leave an input field empty', () => {
-    cy.get('a.contact-card').first().click()
-    cy.get('.name')
+    cy.get('[data-testid=contact-card]').first().click()
+    cy.get('[data-testid=request-name]')
       .type('Joan Russell')
       .should('have.value', 'Joan Russell')
-    cy.get('.button').click()
+    cy.get('[data-testid=request-button]').click()
     cy.location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/contact')
     }));
-   
   })
 
 })

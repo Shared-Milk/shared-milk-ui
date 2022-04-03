d2c03c060b471136e1e19b5cc67625f58b94bc69
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-02 23:29:17 -0600

    Add tests to need milk user flow

--- cypress/integration/need_milk_spec.js
+++ cypress/integration/need_milk_spec.js
@@ -30,5 +30,33 @@ describe('need milk donor results page user flow', () => {
       expect(location.href).to.eq('http://localhost:3000/contact')
     }));
     cy.get('h2').contains('Connect with Stacey Berkowitz')
+    cy.get('form')
+      .get('.main-container')
+      .get('form')
+    cy.get('.name')
+      .type('Joan Russell')
+      .should('have.value', 'Joan Russell')
+      cy.get('.email')
+      .type('j_russell@gmail.com')
+      .should('have.value', 'j_russell@gmail.com')
+      cy.get('.tel')
+      .type('310-810-1022')
+      .should('have.value', '310-810-1022')
+    cy.get('textarea')
+      .type('I love releasing milk and sharing it with babies around the world')
+      .should('have.value', 'I love releasing milk and sharing it with babies around the world')
+    cy.get('.button').click()
+    cy.location().should((location => {
+      expect(location.href).to.eq('http://localhost:3000/sent-confirmation')
+    }));
   })
+
+  it('should display a confirmation message once user clicks submit on milk request form', () => {
+    cy.visit('http://localhost:3000/sent-confirmation')
+    cy.get('h2').contains('Message Sent')
+      .get('p').contains('Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.')
+      .get('p').contains('Best wishes!')
+
+  })
+
 })

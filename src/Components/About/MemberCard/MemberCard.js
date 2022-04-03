25c853b4a3969d2892a3c8b8621d479cf6c11edd
Jani Palomino <palomino.jani@gmail.com>
2022-04-02 23:38:03 -0600

    Add pronouns to profile page

--- src/Components/About/MemberCard/MemberCard.js
+++ src/Components/About/MemberCard/MemberCard.js
@@ -12,6 +12,9 @@ const memberCard = ({member}) => {
         <h4>{member.firstName} {member.lastName}</h4>
         <img className='bio-photo' src={member.photo} alt={`Photo of ${member.firstName}`}/>
       </Link>
+      <div className='pronouns'>
+        <h3>{member.pronouns}</h3>
+      </div>
       <div className='social-links'>
         <a href={member.linkedin} target='_blank'> 
           <img className='social-icon' src={member.linkedinIcon} alt={`${member.firstName}'s Linked In Profile`}/>

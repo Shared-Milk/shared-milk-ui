331c814656fe8e2eedfd0d6356fc18234b4c2cba
Devon Wigle <devonwigle@gmail.com>
2022-04-03 20:28:13 -0500

    Add test ids

--- src/Components/DonateMilkForm/DonateMilkForm.js
+++ src/Components/DonateMilkForm/DonateMilkForm.js
@@ -45,29 +45,29 @@ const DonateMilkForm = () => {
   return (
     <form className='donor-form' onSubmit={ handleSubmit(onSubmit) }>
       <label htmlFor='Your Name'>Your Name</label>
-      <input {...register('donorName', { required: `* Is the baby-brain that bad?` })} type='text' placeholder='First & Last Name' />
-      <p className='error'>{ errors.donorName?.message }</p>
+      <input data-testid='donor-name-input' {...register('donorName', { required: `* Is the baby-brain that bad?` })} type='text' placeholder='First & Last Name' />
+      <p data-testid='donor-name-error' className='error'>{ errors.donorName?.message }</p>
       <label htmlFor='Email Address'>Email Address</label>
-      <input {...register('donorEmail', { required: `* Oh, fer cryin' out loud. We don't do snail mail. Just enter your email.` })} type='email' placeholder='Email Address' />
-      <p className='error'>{ errors.donorEmail?.message }</p>
+      <input data-testid='donor-email-input' {...register('donorEmail', { required: `* Oh, fer cryin' out loud. We don't do snail mail. Just enter your email.` })} type='email' placeholder='Email Address' />
+      <p data-testid='donor-email-error' className='error'>{ errors.donorEmail?.message }</p>
       <label htmlFor='Phone Number'>Phone Number</label>
-      <input {...register('donorPhone', { required: `* Can I have yo number??` })} type='tel' onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()} placeholder='Phone Number' />
-      <p className='error'>{ errors.donorPhone?.message }</p>
+      <input data-testid='donor-phone-input' {...register('donorPhone', { required: `* Can I have yo number??` })} type='tel' onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()} placeholder='Phone Number' />
+      <p data-testid='donor-phone-error' className='error'>{ errors.donorPhone?.message }</p>
       <label htmlFor='Location'>Location</label>
-      <input {...register('donorLocation', { required: `Ope! We can't find you. Peekaboo!` })} type='text' placeholder='Location' />
+      <input data-testid='donor-location-input' {...register('donorLocation', { required: `Ope! We can't find you. Peekaboo!` })} type='text' placeholder='Location' />
+      <p data-testid='donor-location-error' className='error'>{errors.donorLocation?.message}</p>
       <p className='form-question'>Have you used any tobacco products in the last 6 weeks?</p>
       <div className='form-group'>
         <label for='no' htmlFor='no'>NO</label>
-          <input id='no' {...register('smoker', { required: `* Did you go out for a smoke?` })} type='radio' name='smoker' value='0' />
+        <input id='no' data-testid='donor-smoker-no' {...register('smoker', { required: `* Did you go out for a smoke?` })} type='radio' name='smoker' value='0' />
         <label for='yes' htmlFor='yes'>YES</label>
-         <input id='yes' {...register('smoker', { required: `* Did you go out for a smoke?` })} type='radio' name='smoker' value='1' />
-         
+        <input data-testid='donor-smoker-yes' id='yes' {...register('smoker', { required: `* Did you go out for a smoke?` })} type='radio' name='smoker' value='1' />
       </div>
-      <p className='error'>{ errors.smoker?.message }</p>
+      <p data-testid='donor-smoker-error' className='error'>{ errors.smoker?.message }</p>
       <label htmlFor='message'>Message</label>
-      <textarea {...register('donorBio', { required: `* Seriously though, tell us about yourself. Don't be sketchy` })} type='text' placeholder='Tell us about yourself. Why are you donating? How much milk do you have available?' />
-      <p className='error'>{ errors.donorBio?.message }</p>
-      <button className='button' type='submit'>Submit</button>
+      <textarea data-testid='donor-bio-input' {...register('donorBio', { required: `* Seriously though, tell us about yourself. Don't be sketchy` })} type='text' placeholder='Tell us about yourself. Why are you donating? How much milk do you have available?' />
+      <p data-testid='donor-bio-error' className='error'>{ errors.donorBio?.message }</p>
+      <button data-testid='donate-submit-button' className='button' type='submit'>Submit</button>
     </form>
   )
 };

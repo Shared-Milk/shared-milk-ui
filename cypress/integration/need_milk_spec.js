ffc069c846e1dc5907191b0bf747d18456cd4a7d
Jani Palomino <palomino.jani@gmail.com>
2022-04-03 21:50:22 -0600

    Clean up unneeded test ids, and refactor test

--- cypress/integration/need_milk_spec.js
+++ cypress/integration/need_milk_spec.js
@@ -1,4 +1,4 @@
-describe('need milk donor results page user flow', () => {
+describe('Need Milk request user flow', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://secret-forest-87730.herokuapp.com/graphql', (req) => {
       req.reply({
@@ -8,64 +8,80 @@ describe('need milk donor results page user flow', () => {
     })
 
     cy.visit('http://localhost:3000/need-milk')
-  })
+  });
 
-  it('should have a donor results container that displays three donors', () => {
-    cy.get('[data-testid=main-container]').should('exist')
-      .get('[data-testid=contact-grid]')
+  it('Should display a header and footer on page load', () => {
+    cy.get('[data-testid=header]').should('be.visible')
+    cy.get('[data-testid=footer]').should('be.visible')
+  });
+
+  it('Should display a page with 3 elements', () => {
+    cy.get('[data-testid=donors]').should('be.visible').children().should('have.length', 3)
+  });
+
+  it('The first two elements should be a page title and instructions', () => {
+    cy.get('[data-testid=donors] > h2:nth-child(1)').contains('Milk Donors')
+      .get('[data-testid=donors] > p:nth-child(2)').should('have.length', 1)
+  });
+
+  it('Should have a donor results container that displays three donors', () => {
+    cy.get('[data-testid=donors]').should('exist').children()
       .get('[data-testid=contact-card]').should('have.length', 3)
-  })
+  });
  
-  it('should display donor information on each donor card', () => {
+  it('Should display a donor name, location, and a bio on each card', () => {
     cy.get('[data-testid=contact-card]').first()
       .get('[data-testid=donor-name]').contains('Stacey Berkowitz')
       .get('[data-testid=donor-location]').contains('Location: Denver, CO')
       .get('[data-testid=bio-title]').contains('Bio')
       .get('[data-testid=donor-bio]').contains('I want to help struggling mothers and hungry babies')
-  })
+  });
 
-  it('should allow user to click on donor that they want to contact', () => {
+  it('Should allow user to click on donor that they want to contact, display a contact form and allow the user to send a message via that form', () => {
     cy.get('[data-testid=contact-card]').first().click()
-    cy.location().should((location => {
+      .location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/contact')
-    }));
-    cy.get('[data-testid=request-container]')
-    cy.get('[data-testid=donor-connect]').contains('Connect with Stacey Berkowitz')
-    cy.get('[data-testid=request-form]')
-    cy.get('[data-testid=request-name]')
+    }))
+      .get('[data-testid=request-container]').children()
+      .get('h2:nth-child(1)').should('be.visible').contains('Connect with Stacey Berkowitz')
+    cy.get('[data-testid=request-form]').children()
+    cy.get('[for="First & Last Name"]').should('be.visible').contains('Your Name')
+      .get('[name="recipientName"]')
       .type('Joan Russell')
       .should('have.value', 'Joan Russell')
-      cy.get('[data-testid=request-email]')
+    cy.get('[for="Email Address"]').should('be.visible').contains('Email Address')
+      .get('[name="recipientEmail"]')
       .type('j_russell@gmail.com')
       .should('have.value', 'j_russell@gmail.com')
-      cy.get('[data-testid=request-tel]')
+    cy.get('[for="Phone Number"]').should('be.visible').contains('Phone Number')
+      .get('[name="recipientPhone"]')
       .type('3108101022')
       .should('have.value', '3108101022')
-    cy.get('[data-testid=request-bio]')
+    cy.get('[for="Message"]').should('be.visible').contains('Message')
+      .get('[name="recipientMessage"]')
       .type('I need extra milk to feed my growing boy, Teddy')
       .should('have.value', 'I need extra milk to feed my growing boy, Teddy')
     cy.get('[data-testid=request-button]').click()
     cy.location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/sent-confirmation')
     }));
-  })
+  });
 
-  it('should display a confirmation message once user clicks submit on milk request form', () => {
+  it('Should display a confirmation message after a user has submitted a milk request.', () => {
     cy.visit('http://localhost:3000/sent-confirmation')
-    cy.get('[data-testid=confirmation]').contains('Message Sent')
-      .get('[data-testid=confirmation-msg]').contains('Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.')
-      .get('[data-testid=confirmation-wishes]').contains('Best wishes!')
-  })
+    cy.get('[data-testid=confirmation-message] > h2:nth-child(1)').contains('Message Sent')
+      .get('[data-testid=confirmation-message] > p:nth-child(2)').contains('Thank you')
+      .get('[data-testid=confirmation-message] > p:nth-child(3)').contains('Best wishes!')
+  });
 
   it('should not let a user connect with a donor if they leave an input field empty', () => {
     cy.get('[data-testid=contact-card]').first().click()
-    cy.get('[data-testid=request-name]')
+    cy.get('[name="recipientName"]')
       .type('Joan Russell')
       .should('have.value', 'Joan Russell')
     cy.get('[data-testid=request-button]').click()
     cy.location().should((location => {
       expect(location.href).to.eq('http://localhost:3000/contact')
     }));
-  })
-
-})
+  });
+});
--- src/Components/ConfirmationMessages/ConfirmationMessage.js
+++ src/Components/ConfirmationMessages/ConfirmationMessage.js
@@ -1,9 +1,9 @@
 const ConfirmationMessage = () => {
   return (
-    <article className='main-container message'>
-      <h2 data-testid='confirmation'>Message Sent!</h2>
-      <p data-testid='confirmation-msg'>Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.</p>
-      <p data-testid='confirmation-wishes'>Best wishes!</p>
+    <article className='main-container message' data-testid='confirmation-message'>
+      <h2>Message Sent!</h2>
+      <p>Thank you for contacting a donor. Keep an eye on your email or phone for a response from them.</p>
+      <p>Best wishes!</p>
     </article>
   )
 };

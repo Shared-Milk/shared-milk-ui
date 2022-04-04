a637e79ce04ab2f3fca2d25f6c6c1b1ae26803a5
Jani Palomino <palomino.jani@gmail.com>
2022-04-03 23:25:46 -0600

    Move landing page header tests to header and footer test spec, remove unneeded ids

--- cypress/integration/about_page_spec.js
+++ cypress/integration/about_page_spec.js
@@ -10,31 +10,31 @@ describe('About page user flow', () => {
 
   it('Should display a page with 7 elements', () => {
     cy.get('[data-testid=about]').should('be.visible').children().should('have.length', 7)
-  })
+  });
 
   it('The first two elements should be a page title and overview', () => {
-    cy.get('[data-testid="about"] > h2:nth-child(1)').contains('Our Inspiration')
-      .get('[data-testid="about"] > p:nth-child(2)').should('have.length', 1)
-  })
+    cy.get('[data-testid=about] > h2:nth-child(1)').contains('Our Inspiration')
+      .get('[data-testid=about] > p:nth-child(2)').should('have.length', 1)
+  });
 
   it('The third element should be a mission quote with a horizontal rule divider', () => {
     cy.get('[data-testid=about]').children()
       .get('.mission-container').should('be.visible')
-      .get('p').should('have.class', 'mission').contains('"to create a safe space for families who under-produce breast milk to connect with families who over-produce."')
+      .get('p').should('have.class', 'mission').contains('to create a safe space for families who under-produce breast milk to connect with families who over-produce.')
       .get('.dots')
-  })
+  });
 
   it('The fifth element should display our tech stack section and information, with a link to Turing School of Software & Design', () => {
-    cy.get('[data-testid="about"] > :nth-child(5)').should('be.visible').contains('Our Tech Stack')
+    cy.get('[data-testid=about] > :nth-child(5)').should('be.visible').contains('Our Tech Stack')
       .get('p:nth-child(6)').should('be.visible').contains('This is a Turing School of Software & Design')
       .get('[data-testid=turing-link]').should('be.visible').request('https://turing.edu/')
-  })
+  });
 
   it('Should display a Meet the Team container with a title and instructions', () => {
     cy.get('[data-testid=team-container]').should('be.visible').children()
       .get('h3:nth-child(1)').contains('Meet the Team')
-      .get('[data-testid="team-container"] > p').should('have.length', 1)
-  })
+      .get('[data-testid=team-container] > p').should('have.length', 1)
+  });
 
   it('Should display a grid of team member photos, names, pronouns, and social icons', () => {
     cy.get('[data-testid=team-grid]').should('be.visible').children()
@@ -43,22 +43,21 @@ describe('About page user flow', () => {
       .get('.name-and-photo > .bio-photo')
       .get('.pronouns > h3')
       .get('.social-links')
-  })
+  });
 
   it('Should be able to be click on any team members name or photo to visit their bio page', () => {
     cy.get('[data-testid=team-container] > [data-testid=team-grid] > .member-card:nth-child(1) > .name-and-photo > .bio-photo').click().visit('http://localhost:3000/about/seth_perna')
     cy.visit('http://localhost:3000/about')
     cy.get('[data-testid=team-container] > [data-testid=team-grid] > .member-card:nth-child(7) > .name-and-photo > .bio-photo').click().visit('http://localhost:3000/about/devon_wigle')
-  })
+  });
 
   it('Should display a LinkedIn social icon for each person that can be clicked on to visit that team members LinkedIn page', () => {
     cy.get('[data-testid=team-container] > [data-testid=team-grid] > .member-card:nth-child(1)').children()
     .get('.social-links > a:nth-child(1) > .social-icon').should('be.visible').click({ multiple: true })
-
-  })
+  });
 
   it('Should display a Github social icon for each person that can be clicked on to visit that team members Github page', () => {
     cy.get('[data-testid=team-container] > [data-testid=team-grid] > .member-card:nth-child(1)').children()
     .get('.social-links > a:nth-child(2) > .social-icon').should('be.visible').click({ multiple: true })
-  })
-})
+  });
+});
--- cypress/integration/header_footer_spec.js
+++ cypress/integration/header_footer_spec.js
@@ -1,18 +1,85 @@
-describe('Header content', () => {
+import { get } from "react-hook-form";
+
+describe('Header, Nav, and Footer content', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   });
 
-  it('should have header parts', () => {
-    cy.get('[data-testid=site-title]').contains('No Use Cryin\' Over Shared Milk')
-    cy.get('[data-testid=nav-bar]').contains('Home')
-    cy.get('[data-testid=nav-bar]').contains('Need Milk')
-    cy.get('[data-testid=nav-bar]').contains('Donate')
-    cy.get('[data-testid=nav-bar]').contains('About')
-    cy.get('[data-testid=nav-bar]').contains('Support')
+  it('should header and footer on page load', () => {
+    cy.get('[data-testid=header]').should('be.visible')
+    cy.get('[data-testid=footer]').should('be.visible')
+  });
+
+  it('Should have nav bar in header in desktop view, with 5 links', () => {
+    cy.viewport(1532, 897)
+      .get('[data-testid=header]').children()
+      .should('have.class', 'logo')
+      .get('nav').children('.active').should('have.length', 1)
+      .get('.desktop-hidden').should('exist')
+      .get('ul.menu').should('be.visible').children()
+      .get('li').should('have.length', 5)
+      .get('.menu > li:nth-child(1)').contains('Home')
+      .get('.menu > li:nth-child(2)').contains('Need Milk')
+      .get('.menu > li:nth-child(3)').contains('Donate')
+      .get('.menu > li:nth-child(4)').contains('About')
+      .get('.menu > li:nth-child(5)').contains('Support')
+  });
+
+  it('Should be able to navigate all pages on desktop view', () => {
+    cy.viewport(1532, 897)
+      .get('.menu > li:nth-child(1)').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/')
+    }));
+    cy.get('.menu > li:nth-child(2)').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/need-milk')
+    }));
+    cy.get('.menu > li:nth-child(3)').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/donate')
+    }));
+    cy.get('.menu > li:nth-child(4)').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/about')
+    }));
+    cy.get('.menu > li:nth-child(5)').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/support')
+    }));
+  });
+
+  it('should be able to navigate to all pages on mobile view', () => {
+    cy.viewport(1004, 883)
+      .get('.desktop-hidden > div > div > .bm-burger-button > #react-burger-menu-btn').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .active').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .bm-item:nth-child(2)').click().contains('Need Milk').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/need-milk')
+    }));
+    cy.get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .active').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .bm-item:nth-child(3)').click().contains('Donate').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/donate')
+    }));
+    cy.get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .active').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .bm-item:nth-child(4)').click().contains('About').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/about')
+    }));
+    cy.get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .active').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .bm-item:nth-child(5)').click().contains('Support').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/support')
+    }));
+    cy.get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .active').click()
+      .get('div > .bm-menu-wrap > .bm-menu > .bm-item-list > .bm-item:nth-child(1)').click().contains('Home').click()
+      .location().should((location => {
+        expect(location.href).to.eq('http://localhost:3000/')
+    }));
   });
 
   it('should have a footer', () => {
-    cy.get('[data-testid=copyright-line]').should('be.visible')
+    cy.get('[data-testid=footer]').contains('Copyright')
   });
 })

11acd8585a45465b21fb3a2a05e728145db3ec6f
Jani Palomino <palomino.jani@gmail.com>
2022-04-05 12:24:12 -0600

    Add photos to home page and adjust media queries

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,5 +1,8 @@
 import './HomePage.scss';
 import { Link } from 'react-router-dom';
+import Levon_Home from '../../TeamData/KiddoPhotos/Levon_Home.png';
+import Elora_Home from '../../TeamData/KiddoPhotos/Elora_Home.png';
+
 
 const HomePage = () => {
    return (
@@ -9,6 +12,10 @@ const HomePage = () => {
           <p>To create a safe space for families who under-produce breast milk to connect with families who over-produce.</p>
           <p>We keep the money out of it as a donation only based program.</p>
           <p>Whether you are a parent in need, or are here to donate, we hope you are able to build a connection with another family and join us in our mission to help infants thrive by making donated milk accessible.</p>
+        <article className='photo-grid mobile-hidden'>
+          <img className='baby-photo' src={Levon_Home} alt='Baby Levon tummy time'/>
+          <img className='baby-photo' src={Elora_Home} alt='Baby Elora napping'/>
+        </article>
       </aside>
       <aside className='buttons'>
         <Link to='/need-milk' className='need'>Need<br></br>Milk</Link>
